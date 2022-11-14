12e32759158b40546643c6f15a0d16ab00987e50
EC2 Default User <ec2-user@ip-172-31-38-69.ap-northeast-1.compute.internal>
2022-11-14 13:40:57 +0000

    [Add]editページ

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # get 'books/index' => 'books#index'
   # get 'books/show'
   # get 'books/edit'
-  get 'books/:id/edit' => 'books#edit', as:'edit_list'
+  patch 'books/:id' => 'books#update', as:'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
