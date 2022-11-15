99172befa3f8828a20017150a205637023acb8af
EC2 Default User <ec2-user@ip-172-31-38-69.ap-northeast-1.compute.internal>
2022-11-15 09:11:32 +0000

    [Add]destroy機能

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   # get 'books/show'
   # get 'books/edit'
   patch 'books/:id' => 'books#update', as:'update_book'
+  delete 'books/:id' => 'books#destroy', as:'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
